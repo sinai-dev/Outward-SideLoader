7a1b538cafe335ffac229aa9627a8bceb5c2a339
sinaioutlander <49360850+sinaioutlander@users.noreply.github.com>
2020-06-12 20:40:38 +1000

    2.4.0

--- SideLoader/SideLoader/SL.cs
+++ SideLoader/SideLoader/SL.cs
@@ -23,7 +23,7 @@ namespace SideLoader
         // Mod Info
         public const string GUID = "com.sinai." + MODNAME;
         public const string MODNAME = "SideLoader";
-        public const string VERSION = "2.3.0";
+        public const string VERSION = "2.4.0";
 
         // Folders
         public static string PLUGINS_FOLDER => Paths.PluginPath;
