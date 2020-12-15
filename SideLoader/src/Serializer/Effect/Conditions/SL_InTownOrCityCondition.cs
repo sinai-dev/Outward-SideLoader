4330a7124abe6e4529a4837ac9ce93bcddac1408
sinaioutlander <49360850+sinaioutlander@users.noreply.github.com>
2020-12-15 17:32:37 +1100

    Added some missing minor classes

---
+++ SideLoader/src/Serializer/Effect/Conditions/SL_InTownOrCityCondition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SideLoader
+{
+    public class SL_InTownOrCityCondition : SL_EffectCondition
+    {
+        public override void ApplyToComponent<T>(T component)
+        {
+        }
+
+        public override void SerializeEffect<T>(T component)
+        {
+        }
+    }
+}
---
+++ SideLoader/src/Serializer/Effect/Effects/SL_AddPukeOnEat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SideLoader
+{
+    public class SL_AddPukeOnEat : SL_Effect
+    {
+        public override void ApplyToComponent<T>(T component)
+        {
+            
+        }
+
+        public override void SerializeEffect<T>(T effect)
+        {
+            
+        }
+    }
+}
