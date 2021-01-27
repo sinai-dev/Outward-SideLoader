21372225ea84f28f032dadf67da9bfb51def7a70
Sinai <outlander.sinai@gmail.com>
2021-01-28 04:33:14 +1100

    Add some more helpers to SLPack for getting content

--- src/SLPacks/SLPack.cs
+++ src/SLPacks/SLPack.cs
@@ -1,15 +1,9 @@
-﻿using System;
+﻿using SideLoader.SLPacks;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using UnityEngine;
 using System.IO;
-using SideLoader.Helpers;
-using SideLoader.Model;
-using SideLoader.SaveData;
-using SideLoader.Model.Status;
-using System.Collections;
-using SideLoader.SLPacks;
+using System.Linq;
+using UnityEngine;
 using SLPackContent = System.Collections.Generic.Dictionary<System.Type, System.Collections.Generic.Dictionary<string, object>>;
 
 namespace SideLoader
@@ -33,8 +27,32 @@ namespace SideLoader
             $@"{SL.SL_FOLDER}\{Name}" :
             $@"{SL.PLUGINS_FOLDER}\{Name}\SideLoader";
 
-        public string GetPathForCategory<T>() where T : SLPackCategory => GetPathForCategory(typeof(T));
+        // Hard-coded dictionaries for easy access:
 
+        /// <summary>AssetBundles loaded from the `AssetBundles\` folder. Dictionary Key is the file name.</summary>
+        public Dictionary<string, AssetBundle> AssetBundles = new Dictionary<string, AssetBundle>();
+        /// <summary>Texture2Ds loaded from the PNGs in the `Texture2D\` folder (not from the `Items\...` folders). Dictionary Key is the file name (without ".png")</summary>
+        public Dictionary<string, Texture2D> Texture2D = new Dictionary<string, Texture2D>();
+        /// <summary>AudioClips loaded from the WAV files in the `AudioClip\` folder. Dictionary Key is the file name (without ".wav")</summary>
+        public Dictionary<string, AudioClip> AudioClips = new Dictionary<string, AudioClip>();
+        /// <summary>SL_Characters loaded from the `Characters\` folder. Dictionary Key is the file name without xml.</summary>
+        public Dictionary<string, SL_Character> CharacterTemplates = new Dictionary<string, SL_Character>();
+
+        internal SLPackContent LoadedContent = new SLPackContent();
+
+        /// <summary>
+        /// Returns the folder path for this SLPack, plus the given SLPackCategory's FolderPath.
+        /// </summary>
+        /// <typeparam name="T">The SLPackCategory type.</typeparam>
+        /// <returns>The path string, or null if an error was experienced.</returns>
+        public string GetPathForCategory<T>() where T : SLPackCategory
+            => GetPathForCategory(typeof(T));
+
+        /// <summary>
+        /// Returns the folder path for this SLPack, plus the given SLPackCategory's FolderPath.
+        /// </summary>
+        /// <param name="type">The SLPackCategory type.</param>
+        /// <returns>The path string, or null if an error was experienced.</returns>
         public string GetPathForCategory(Type type)
         {
             if (type == null || !typeof(SLPackCategory).IsAssignableFrom(type))
@@ -47,11 +65,64 @@ namespace SideLoader
             return $@"{this.FolderPath}\{instance.FolderName}";
         }
 
-        internal SLPackContent LoadedContent = new SLPackContent();
+        /// <summary>
+        /// Get content of type T by the file name. If there are duplicates, it will return the first found (unknown order).<br/><br/>
+        /// Note: This will <b>not</b> find "extra" assets such as the 'icon.png' and Textures\ files from an SL_Item template, etc.
+        /// </summary>
+        /// <typeparam name="T">The type of content you're looking for</typeparam>
+        /// <param name="fileName">The content's file name (without extension)</param>
+        /// <param name="stringComparison">The string comparison type to use, default is to culture- and case-insensitive.</param>
+        /// <returns></returns>
+        public T GetContentByFileName<T>(string fileName, StringComparison stringComparison = StringComparison.InvariantCultureIgnoreCase)
+        {
+            var content = GetContentOfType<T>();
 
+            if (content == null || !content.Any())
+                return default;
+
+            var query = content.Where(it => string.Equals(Path.GetFileName(it.Key), fileName, stringComparison))
+                               .Select(it => it.Value);
+
+            return query.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Get all loaded content of type T from the SL Pack. The return dictionary keys are the file paths.<br/><br/>
+        /// Note: This will <b>not</b> find "extra" assets such as the 'icon.png' and Textures\ files from an SL_Item template, etc.
+        /// </summary>
+        /// <typeparam name="T">The type of content to load from the SL Pack.</typeparam>
+        /// <returns>A dictionary of the loaded content if any found, otherwise null. Keys are the file paths, Values are the content.</returns>
+        public Dictionary<string, T> GetContentOfType<T>()
+        {
+            var ret = new Dictionary<string, T>();
+
+            foreach (var ctg in LoadedContent)
+            {
+                foreach (var entry in ctg.Value)
+                {
+                    if (typeof(T).IsAssignableFrom(entry.Value.GetType()))
+                        ret.Add(entry.Key, (T)entry.Value);
+                }
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Get the loaded content from this SL Pack from the given category. Categories are in the "SideLoader.SLPacks.Categories" namespace.<br/><br/>
+        /// Note: This will <b>not</b> find "extra" assets such as the 'icon.png' and Textures\ files from an SL_Item template, etc.
+        /// </summary>
+        /// <typeparam name="T">The SLPackCategory type to get content for.</typeparam>
+        /// <returns>A dictionary of the loaded content if any found, otherwise null. Keys are the file paths, Values are the content.</returns>
         public Dictionary<string, object> GetContentForCategory<T>() where T : SLPackCategory
             => GetContentForCategory(typeof(T));
 
+        /// <summary>
+        /// Get the loaded content from this SL Pack from the given category. Categories are in the "SideLoader.SLPacks.Categories" namespace.<br/><br/>
+        /// Note: This will <b>not</b> find "extra" assets such as the 'icon.png' and Textures\ files from an SL_Item template, etc.
+        /// </summary>
+        /// <param name="type">The SLPackCategory type to get content for.</param>
+        /// <returns>A dictionary of the loaded content if any found, otherwise null. Keys are the file paths, Values are the content.</returns>
         public Dictionary<string, object> GetContentForCategory(Type type)
         {
             if (type == null || !typeof(SLPackCategory).IsAssignableFrom(type))
@@ -64,15 +135,6 @@ namespace SideLoader
 
             return ret;
         }
-
-        /// <summary>AssetBundles loaded from the `AssetBundles\` folder. Dictionary Key is the file name.</summary>
-        public Dictionary<string, AssetBundle> AssetBundles = new Dictionary<string, AssetBundle>();
-        /// <summary>Texture2Ds loaded from the PNGs in the `Texture2D\` folder (not from the `Items\...` folders). Dictionary Key is the file name (without ".png")</summary>
-        public Dictionary<string, Texture2D> Texture2D = new Dictionary<string, Texture2D>();
-        /// <summary>AudioClips loaded from the WAV files in the `AudioClip\` folder. Dictionary Key is the file name (without ".wav")</summary>
-        public Dictionary<string, AudioClip> AudioClips = new Dictionary<string, AudioClip>();
-        /// <summary>SL_Characters loaded from the `Characters\` folder. Dictionary Key is the file name without xml.</summary>
-        public Dictionary<string, SL_Character> CharacterTemplates = new Dictionary<string, SL_Character>();
 
         internal void TryApplyItemTextureBundles()
         {
