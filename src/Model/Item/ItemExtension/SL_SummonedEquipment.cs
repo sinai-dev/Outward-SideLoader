57d9cc1b24b3500f1b2fd848b964d55b94b7bf6c
Sinai <outlander.sinai@gmail.com>
2021-03-24 20:31:41 +1100

    SL_SummonedEquipment

---
+++ src/Model/Item/ItemExtension/SL_SummonedEquipment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SideLoader
+{
+    // Currently has no fields, just a dummy.
+    // TODO: Support the 'Sprite' field.
+    public class SL_SummonedEquipment : SL_ItemExtension
+    {
+        public override void ApplyToComponent<T>(T component)
+        {
+            
+        }
+
+        public override void SerializeComponent<T>(T extension)
+        {
+            
+        }
+    }
+}
